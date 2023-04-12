155f062dc47e68565bdb94e5367255e264a5060a
Richard Yen <richyen3@gmail.com>
2023-04-11 23:37:28 -0700

    keep things running instead of exiting

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,6 @@ service postgresql start
 cd /cloudsmith_fdw && python3 setup.py install
 sed "s/your_secret_api_key/${CLOUDSMITH_TOKEN}/" /cloudsmith_fdw/packages.sample.sql | psql postgres postgres
 psql -c "SELECT * FROM cloudsmith.packages LIMIT 10" postgres postgres
+
+# Keep things running
+touch /var/log/messages && tail -f /var/log/messages
