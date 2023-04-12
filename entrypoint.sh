e93546c23dd1679b7c73398475c4a518ef0364ea
Richard Yen <richyen3@gmail.com>
2023-04-11 23:44:20 -0700

    tidy up -- move sql files into a folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 service postgresql start
 cd /cloudsmith_fdw && python3 setup.py install
-sed "s/your_secret_api_key/${CLOUDSMITH_TOKEN}/" /cloudsmith_fdw/packages.sample.sql | psql postgres postgres
+sed "s/your_secret_api_key/${CLOUDSMITH_TOKEN}/" /cloudsmith_fdw/sample_sql/packages.sample.sql | psql postgres postgres
 psql -c "SELECT * FROM cloudsmith.packages LIMIT 10" postgres postgres
 
 # Keep things running
